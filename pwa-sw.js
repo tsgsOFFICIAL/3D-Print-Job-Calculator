5c2cb814c8e634950f0469b0d6a5a1f056bc2cfa
Marcus Jensen <mmj@dnp.dk>
2024-08-08 12:56:10 +0200

    updated sources

--- pwa-sw.js
+++ pwa-sw.js
@@ -1,32 +1,32 @@
-const version = '0.0.1';
+const version = '0.0.2';
 const cacheName = '3d-print-cost-calculator-cache';
 const assets = [
-    '/',
-    '/manifest.json',
-    '/assets/img/icon-close-min.png',
-    '/assets/img/icon-close.png',
-    '/assets/img/logo-min.png',
-    '/assets/img/logo.png',
-    '/assets/pwa/icons/128x128.png',
-    '/assets/pwa/icons/144x144.png',
-    '/assets/pwa/icons/152x152.png',
-    '/assets/pwa/icons/192x192.png',
-    '/assets/pwa/icons/384x384.png',
-    '/assets/pwa/icons/512x512.png',
-    '/assets/pwa/icons/72x72.png',
-    '/assets/pwa/icons/96x96.png',
-    '/assets/pwa/screenshots/screenshot 1.png',
-    '/assets/pwa/screenshots/screenshot 2.png',
-    '/css/animations.css',
-    '/css/blur-load.css',
-    '/css/main.css',
-    '/css/pwa.css',
-    '/css/toast.css',
-    '/css/variables.css',
-    '/js/cookie.js',
-    '/js/img-loader.js',
-    '/js/main.js',
-    '/js/notifications.js',
+    './',
+    './manifest.json',
+    './assets/img/icon-close-min.png',
+    './assets/img/icon-close.png',
+    './assets/img/logo-min.png',
+    './assets/img/logo.png',
+    './assets/pwa/icons/128x128.png',
+    './assets/pwa/icons/144x144.png',
+    './assets/pwa/icons/152x152.png',
+    './assets/pwa/icons/192x192.png',
+    './assets/pwa/icons/384x384.png',
+    './assets/pwa/icons/512x512.png',
+    './assets/pwa/icons/72x72.png',
+    './assets/pwa/icons/96x96.png',
+    './assets/pwa/screenshots/screenshot 1.png',
+    './assets/pwa/screenshots/screenshot 2.png',
+    './css/animations.css',
+    './css/blur-load.css',
+    './css/main.css',
+    './css/pwa.css',
+    './css/toast.css',
+    './css/variables.css',
+    './js/cookie.js',
+    './js/img-loader.js',
+    './js/main.js',
+    './js/notifications.js',
 ];
 
 // Cache all the files to make a PWA
@@ -69,7 +69,7 @@ self.addEventListener("fetch", fetchEvent => {
         }).catch(async () => {
             // If the network is unavailable, use the cache
             const cacheResponse = await caches.match(fetchEvent.request);
-            return cacheResponse || caches.match('/error/404');
+            return cacheResponse || caches.match('./error/404');
         })
     );
 });
